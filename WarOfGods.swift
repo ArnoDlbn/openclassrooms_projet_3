4f882b67e5f729953046fe16d34d3ecb6dca0233
Arnaud Dalbin <arnaud.dalbin@gmail.com>
2019-02-24 15:38:16 +0100

    Step 3 and Bonus complete

--- WarOfGods.swift
+++ WarOfGods.swift
@@ -1,55 +1,166 @@
-// La classe mère Personnage qui régie les classes des différents personnages possible dans le jeu
+// The class which runs all different types of characters in the game
 class Character {
     var name: String
     var life: Int
     var attack: Int
     var heal: Int
+    var weapon: Weapon?
+    var element: Element
     
-    init(name: String, life: Int, attack: Int, heal: Int) {
+    init(name: String, life: Int, attack: Int, heal: Int, element: Element) {
         self.name = name
         self.life = life
         self.attack = attack
         self.heal = heal
+        self.element = element
+    }
+    
+    var realAttack: Int {
+        return self.attack + (self.weapon?.attack ?? 0)
+    }
+    
+    var realHeal: Int {
+        return self.heal + (self.weapon?.heal ?? 0)
     }
 }
 
-// La classe du guerrier
+// The warrior class
 class Warrior: Character {
     
-    init(name: String) {
-        super.init(name: name, life: 100, attack: 10, heal: 0)
+    init(name: String, element: Element) {
+        super.init(name: name, life: 100, attack: 10, heal: 0, element: element)
     }
-    
 }
 
-// La classe du mage
+// The wizard class
 class Wizard: Character {
     
-    init(name: String) {
-        super.init(name: name, life: 75, attack: 0, heal: 10)
+    init(name: String, element: Element) {
+        super.init(name: name, life: 75, attack: 0, heal: 10, element: element)
     }
-    
 }
 
-// La classe du colosse
+// The giant class
 class Giant: Character {
     
-    init(name: String) {
-        super.init(name: name, life: 200, attack: 5, heal: 0)
+    init(name: String, element: Element) {
+        super.init(name: name, life: 200, attack: 5, heal: 0, element: element)
     }
-    
 }
 
-// La classe du nain
+// The dwarf class
 class Dwarf: Character {
     
-    init(name: String) {
-        super.init(name: name, life: 50, attack: 20, heal: 0)
+    init(name: String, element: Element) {
+        super.init(name: name, life: 50, attack: 20, heal: 0, element: element)
     }
-    
 }
 
-// La classe du Joueur avec son nom et son équipe constituée
+// The class which runs all different types of weapons in the game
+class Weapon {
+    var attack: Int
+    var heal: Int
+    var name: String
+    
+    init(name: String, attack: Int, heal: Int) {
+        self.attack = attack
+        self.heal = heal
+        self.name = name
+    }
+}
+
+// The class of weapons that attack
+class AttackingWeapon: Weapon {
+    
+    init(name: String, attack: Int) {
+        super.init(name: name, attack: attack, heal: 0)
+    }
+}
+
+// The class of weapons that heal
+class HealingWeapon: Weapon {
+    
+    init(name: String, heal: Int) {
+        super.init(name: name, attack: 0, heal: heal)
+    }
+}
+
+// The enumeration which controls the elements of the characters and their interactions
+enum Element {
+    case fire
+    case air
+    case water
+    case earth
+    
+    var lowestElements: [Element] {
+        switch self {
+        case .fire: return [.air]
+        case .air: return [.earth]
+        case .water: return [.fire]
+        case .earth: return [.water]
+        }
+    }
+    
+    var highestElements: [Element] {
+        switch self {
+        case .earth: return [.air]
+        case .fire: return [.water]
+        case .air: return [.fire]
+        case .water: return [.earth]
+        }
+    }
+    
+    // Allows to know the coefficient of strenght between two elements for healing
+    func coefficientToApplyOnHeal(_ element: Element) -> Double {
+        if self == element {
+            return 1.2
+        } else {
+            return 1.0
+        }
+    }
+    
+    // Allows to know the coefficient of strenght between two elements for attacking
+    func coefficientToApplyOnAttack(_ element: Element) -> Double {
+        if self.lowestElements.contains(element) {
+            return 1.2
+        } else if self.highestElements.contains(element) {
+            return 0.8
+        } else {
+            return 1.0
+        }
+    }
+}
+
+// The class which stores all the weapons in a chest
+class Chest {
+    var toothbrush: AttackingWeapon
+    var sword: AttackingWeapon
+    var axe: AttackingWeapon
+    var toothpick: HealingWeapon
+    var staff: HealingWeapon
+    var orb: HealingWeapon
+    var weapons: [Weapon]
+    
+    init() {
+        self.toothbrush = AttackingWeapon(name: "une brosse à dent", attack: 1)
+        self.sword = AttackingWeapon(name: "une épée", attack: 5)
+        self.axe = AttackingWeapon(name: "une hache", attack: 10)
+        self.toothpick = HealingWeapon(name: "un cure dent", heal: 1)
+        self.staff = HealingWeapon(name: "un bâton", heal: 5)
+        self.orb = HealingWeapon(name: "un orbe", heal: 10)
+        self.weapons = [toothbrush, sword, axe, toothpick, staff, orb]
+        
+    }
+    
+    // Picks a random weapon
+    func pickWeapon() -> Weapon {
+        let weapon = weapons.randomElement()
+        print("Le coffre contient \(weapon!.name)")
+        return weapon!
+    }
+}
+
+// The class with all the informations and functions about the player and his team of characters
 class Player {
     var name: String
     var characters: [Character]
@@ -63,57 +174,42 @@ class Player {
         showTeam()
     }
     
-    // Création d'une équipe
+    // Team building
     func makeTeam() {
         while characters.count < 3 {
             print("\nChoisissez un personnage:"
-                + "\n1. Warrior"
-                + "\n2. Wizard"
-                + "\n3. Giant"
-                + "\n4. Dwarf")
+                + "\n1. Guerrier"
+                + "\n2. Magicien"
+                + "\n3. Géant"
+                + "\n4. Nain")
             if let choice = readLine() {
-                
-                //                var characterClass: Character.Type
-                //                switch choice {
-                //                case "1":
-                //                    characterClass = Warrior.self
-                //                case "2":
-                //                    characterClass = Wizard.self
-                //                case "3":
-                //                    characterClass = Giant.self
-                //                case "4":
-                //                    characterClass = Dwarf.self
-                //                default:
-                //                    break
-                //                }
-                //
-                //                print("Quel est le nom du \(characterClass) ?")
-                //                name = readUniqueName()
-                //                var character = characterClass.init(name: name)
-                //                characters ["\(name)"] = character
-                
                 var character: Character
                 var characterName = String()
+                var characterElement = Element.fire
                 switch choice {
                 case "1" :
-                    print("\nQuel est le nom du Warrior ?")
+                    print("\nQuel est le nom du Guerrier ?")
                     characterName = readUniqueName()
-                    character = Warrior(name: characterName)
+                    characterElement = chooseElement()
+                    character = Warrior(name: characterName, element: characterElement)
                     characters.append(character)
                 case "2" :
-                    print("\nQuel est le nom du Wizard ?")
+                    print("\nQuel est le nom du Magicien ?")
                     characterName = readUniqueName()
-                    character = Wizard(name: characterName)
+                    characterElement = chooseElement()
+                    character = Wizard(name: characterName, element: characterElement)
                     characters.append(character)
                 case "3" :
-                    print("\nQuel est le nom du Giant ?")
+                    print("\nQuel est le nom du Géant ?")
                     characterName = readUniqueName()
-                    character = Giant(name: characterName)
+                    characterElement = chooseElement()
+                    character = Giant(name: characterName, element: characterElement)
                     characters.append(character)
                 case "4" :
-                    print("\nQuel est le nom du Dwarf ?")
+                    print("\nQuel est le nom du Nain ?")
                     characterName = readUniqueName()
-                    character = Dwarf(name: characterName)
+                    characterElement = chooseElement()
+                    character = Dwarf(name: characterName, element: characterElement)
                     characters.append(character)
                 default :
                     print("Pouvez-vous répéter ?")
@@ -121,11 +217,10 @@ class Player {
                 }
                 Player.characterNames.append(characterName)
             }
-            
         }
     }
     
-    // Vérification de l'unicité du nom donné
+    // Checks the uniqueness of the given name
     func readUniqueName() -> String {
         let uniqueName: String? = readLine()
         if uniqueName != nil {
@@ -142,18 +237,44 @@ class Player {
         return uniqueName!
     }
     
-    // Présentation de l'équipe du joueur
+    // Picks an element
+    func chooseElement() -> Element {
+        var element = Element.fire
+        print("\nDe quel élément voulez-vous qu'il soit ?"
+            + "\n1. Feu"
+            + "\n2. Eau"
+            + "\n3. Air"
+            + "\n4. Terre")
+        if let choice = readLine() {
+            switch choice {
+            case "1" :
+                element = .fire
+            case "2" :
+                element = .water
+            case "3" :
+                element = .air
+            case "4" :
+                element = .earth
+            default :
+                print("Pouvez-vous répéter ?")
+                return chooseElement()
+            }
+        }
+        return element
+    }
+    
+    // Introduces the player's team
     func showTeam() {
         var i = 1
         for character in characters {
-            print("\(i). \(character.name) qui est un \(type(of: character))")
+            print("\(i). \(character.name) est un \(type(of: character)) de type \(character.element)")
             i += 1
         }
     }
     
-    // Choisir un personnage dans l'équipe du joueur et vérifie qu'il soit vivant
+    // Picks a character in the player's team and checks if he is alive
     func pickCharacter() -> Character {
-        print("\nChoisissez un personnage.")
+        print("\n\(self.name), choisissez un personnage.")
         showTeam()
         let choice: String? = readLine()
         if choice != "1" && choice != "2" && choice != "3" {
@@ -169,7 +290,7 @@ class Player {
         return characters[index! - 1]
     }
     
-    // Vérifier si l'équipe est vivante
+    // Checks if the team is alive
     func isTeamAlive() -> Bool {
         var life: Int = 0
         for character in characters {
@@ -180,35 +301,24 @@ class Player {
         } else {
             return true
         }
-        // return life != 0
     }
-    
 }
 
-// La classe Jeu régissant la partie et la création des joueurs, ainsi que leur nomination
+// La classe Jeu régissant la partie, la création des joueurs, leur nomination et le combat avec l'apparition aléatoire d'un coffre
 class Game {
     var players: [Player]
     
     init() {
         self.players = []
+        print("Bonjour et bienvenu dans War Of Gods !")
         addPlayer()
         addPlayer()
         fight()
     }
     
-    // Ajout d'un joueur à la partie
+    // Adds a player to the game
     func addPlayer() {
-        
-        //        var name: String?
-        //        repeat {
-        //            print("Quel est le nom du joueur \(players.count + 1) ?")
-        //            name = readLine()
-        //        } while name != nil && name != "";
-        //
-        //        players.append(Player(name: name!))
-        
-        print("Bonjour et bienvenu dans War Of Gods !"
-            + "\nQuel est le nom du joueur \(players.count + 1) ?")
+        print("\nQuel est le nom du joueur \(players.count + 1) ?")
         if let name = readLine(), name != "" {
             players.append(Player(name: name))
         } else {
@@ -217,25 +327,43 @@ class Game {
         }
     }
     
-    // Déroulé du combat
+    // All stages of the fight
     func fight() {
         var character1: Character
         var character2: Character
+        var chest: Chest?
+        var weapon: Weapon
         var i: Int = 0
         print("\nLe combat va débuter."
             + "\n\(players[i].name) veuillez commencer.")
         while players[0].isTeamAlive() && players[1].isTeamAlive() {
             character1 = players[i % 2].pickCharacter()
+            chest = makeChestAppear()
+            if chest != nil {
+                weapon = chest!.pickWeapon()
+                if type(of: weapon) == HealingWeapon.self && type(of: character1) == Wizard.self {
+                    character1.weapon = weapon
+                    print("\n\(character1.name) s'équipe de l'arme."
+                        + "\nSon pouvoir de soigner s'élève maintenant à \(character1.realHeal) points de vie.")
+                } else if type(of: weapon) == AttackingWeapon.self && type(of: character1) != Wizard.self {
+                    character1.weapon = weapon
+                    print("\n\(character1.name) s'équipe de l'arme."
+                        + "\nSon attaque s'élève maintenant à \(character1.realAttack) points d'attaque.")
+                }
+            }
             if type(of: character1) == Wizard.self {
-                print("\(character1.name) est un Wizard, vous pouvez donc soigner quelqu'un de votre équipe.")
+                print("\n\(character1.name) est un Magicien, vous pouvez donc soigner quelqu'un de votre équipe.")
                 character2 = players[i % 2].pickCharacter()
-                character2.life += character1.heal
-                print("\(character2.name) a récupéré \(character1.heal) points de vie, il en a maintenant \(character2.life)")
+                let heal = Int(Double(character1.realHeal) * character1.element.coefficientToApplyOnHeal(character2.element))
+                character2.life += heal
+                print("\(character2.name) a récupéré \(heal) points de vie, il en a maintenant \(character2.life)")
             } else {
-                print("\(character1.name) est un \(type(of: character1)), vous pouvez donc attaquer quelqu'un de l'équipe adverse.")
+                print("\n\(character1.name) est un \(type(of: character1)), vous pouvez donc attaquer quelqu'un de l'équipe adverse.")
                 character2 = players[(i + 1) % 2].pickCharacter()
-                character2.life -= character1.attack
-                print("\(character2.name) a perdu \(character1.attack) points de vie, il lui en reste \(character2.life).")
+                let damage = Int(Double(character1.realAttack) * character1.element.coefficientToApplyOnAttack(character2.element))
+                character2.life -= damage
+                print("\(character2.name) a perdu \(damage) points de vie, il lui en reste \(character2.life).")
+                
                 if character2.life < 0 {
                     character2.life = 0
                     print("\(character2.name) est mort !")
@@ -250,7 +378,18 @@ class Game {
         }
     }
     
+    // Random appearance of a chest
+    func makeChestAppear() -> Chest? {
+        let random = [0,1]
+        let randomElement = random.randomElement()
+        if randomElement == 1 {
+            print("\nUn coffre apparaît !")
+            return Chest()
+        } else {
+            print("\nPas de coffre pour cette fois ci ...")
+            return nil
+        }
+    }
 }
 
 var game = Game()
-
